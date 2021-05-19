45c5bb299c7531ec137fb8fd5814ea12cac5bb76
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-19 21:47:50 +0600

    ok 20

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ app.get('/', (req, res) => {
 })
 console.log(process.env.DB_USER)
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("volunteer-network").collection("events");
   // perform actions on the collection object
   client.close();
+  console.log(err)
 });
 
 
