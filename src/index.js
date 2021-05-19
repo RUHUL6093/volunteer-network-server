6c2dd87c003a9b40c57600746a9a45787d8f4bdb
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-19 22:50:19 +0600

    connected mongodb

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-const express = require('express')
-const app = express()
-const cors =require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 app.use(cors());
-const port = 5000
-require('dotenv').config()
+const port = 5000;
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-console.log(process.env.DB_USER)
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+console.log(process.env.DB_USER);
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect((err) => {
   const collection = client.db("volunteer-network").collection("events");
-  // perform actions on the collection object
-  client.close();
-  console.log(err)
+  app.get("/events", (req, res) => {
+    collection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/addEvents", (req, res) => {
+    const events = req.body;
+    collection.insertMany(events, (err, result) => {
+      console.log(err, result);
+    });
+  });
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 });
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port);
