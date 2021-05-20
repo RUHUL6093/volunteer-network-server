7c8a9286c0766bd81878db259c414d25cb625c6d
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-20 09:09:59 +0600

    new node

--- src/index.js
+++ src/index.js
@@ -2,37 +2,24 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
-app.use(express.json());
 const port = 5000;
-require("dotenv").config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-console.log(process.env.DB_USER);
+
 const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${processs.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 client.connect((err) => {
-  const collection = client.db("volunteer-network").collection("events");
-  app.get("/events", (req, res) => {
-    collection.find({}).toArray((err, documents) => {
-      res.send(documents);
-    });
-  });
-  app.post("/addEvents", (req, res) => {
-    const events = req.body;
-    collection.insertMany(events, (err, result) => {
-      console.log(err, result);
-      res.send({ count: result });
-    });
-  });
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+  const eventscollection = client.db("volunteer-network").collection("events");
+  // perform actions on the collection object
+  console.log(err);
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
