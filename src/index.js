3d259cd8dee73f89e53c9cd21adc766457d023a0
RUHUL AMIN <59334063+RUHUL6093@users.noreply.github.com>
2021-05-20 09:29:16 +0600

    ok not

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ const cors = require("cors");
 app.use(cors());
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const MongoClient = require("mongodb").MongoClient;
-const uri = `mongodb+srv://${processs.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g4xsc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 client.connect((err) => {
-  const eventscollection = client.db("volunteer-network").collection("events");
+  const eventsCollection = client.db("volunteer-network").collection("events");
+
+  app.get("/events", (req, res) => {
+    eventsCollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+
   // perform actions on the collection object
-  console.log(err);
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 });
 
 app.listen(port, () => {
